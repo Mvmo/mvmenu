9de0f25e31de2e1958e568ff8459ff7f48c7201b
Mvmo <mvmogo@gmail.com>
2022-11-24 19:54:11 +0100

    Add todos

--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import * as Uebersicht from "uebersicht";
 
+// TODO: Better variant of styling
 export const className = `
     background-color: black;
     width: 100vw;
@@ -7,28 +8,32 @@ export const className = `
     font-family: Monaco;
 `
 
+// TODO: signals?
 export const refreshFrequency = 6000;
 
+// TODO: relative path
 export const command = "bash /Users/mvmo/.dotfiles/.config.d/uebersicht/widgets/mvmenu/libs/scripts/init.sh"
 
 const SpaceCard = ({ space }) => {
     return (
         <div style={{
             backgroundColor: space["has-focus"] ? "red" :"gray",
-                display: "inline-block",
-                padding: 7,
-                marginRight: 10
+            display: "inline-block",
+            padding: 7,
+            marginRight: 10,
         }}>
             <span>{space.label || space.index}</span>
         </div>
     )
 }
 
+// TODO: Better error handling
 export const render = ({ output, error }) => {
     if (!output)
         return (
             <span>Error no output</span>
         )
+
     const data = JSON.parse(output);
     if (!data)
         return (
